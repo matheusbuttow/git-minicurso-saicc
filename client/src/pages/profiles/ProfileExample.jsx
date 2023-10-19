9d2dc080702c1d1e417cabfbc11f3181c42200fc
matheusbuttow <63423891+matheusbuttow@users.noreply.github.com>
2023-10-19 11:49:50 -0300

    Update ProfileExample.jsx
    matheus

--- client/src/pages/profiles/ProfileExample.jsx
+++ client/src/pages/profiles/ProfileExample.jsx
@@ -7,7 +7,7 @@ import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlin
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 
-export default function TestProfile() {
+export default function ProfileExample() {
 
   return (
     <div className="flex flex-col min-h-screen border-none screen rounded-lg bg-white px-4 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
@@ -19,7 +19,7 @@ export default function TestProfile() {
                 <div className="absolute -bottom-6">
                   {/* Sua foto aqui: */}
                     <img
-                      src="https://www.instagram.com/p/CxTX5jnPWIv/?img_index=1"
+                      src="https://stcotvfoco.com.br/2023/01/Capturar-50-1-e1674361788456.png"
                       className="object-cover border-4 border-white w-40 h-40 rounded-full"
                       alt="cover"
                     />
@@ -28,15 +28,16 @@ export default function TestProfile() {
               <div className="text-center mt-12">
                 {/* Seu nome aqui: */}
                 <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-                  Breno Soares
+                  Matheus Buttow
                 </h3>
 
                 <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
                   <MapOutlinedIcon style={{color: '#FDAE17'}}/>                                           
                   {/* Sua cidade aqui: */}
                   <span className="align-items-center" style={{marginLeft: '5px'}}>
-                    Rio Grande, RS
+                    Canguçu, RS
                   </span>
+                  <br/>
                 </div>
 
                 <div className="mb-2 text-blueGray-600 mt-10">
@@ -51,7 +52,7 @@ export default function TestProfile() {
                   <SchoolOutlinedIcon style={{color: '#FDAE17'}}/>                        
                   {/* O curso aqui: */}
                   <span className="align-items-center" style={{marginLeft: '5px'}}>
-                    Engenharia de Computação
+                    eng. de comp
                   </span>
                 </div>
               </div>
@@ -61,12 +62,19 @@ export default function TestProfile() {
                   <div className="w-full lg:w-9/12 px-4">                           
                     {/* Biografia aqui: */}
                     <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                    Oi, pessoal, eu sou o Breno Soares, tenho 21 anos e sou obcecado por gatos! Quando não estou correndo atrás deles, estou ganhando várias partidas no rank mestre de League of Legends. 
-                    Também sou um torcedor fervoroso do Santos Futebol Clube, que é, obviamente, o melhor time do mundo.
+                     fala rapazeada, meu nome é matheus. Sou torcedor do tri-campeão da América
+                  
                     </p>
                     <br/>
+                    <img
+                      src="https://www.rbsdirect.com.br/imagesrc/25219958.jpg?w=700"
+                    />
+                    <br/>
                     {/* Link para seu GitHub aqui: */}
-                    <a href="https://github.com/romulodm/git-minicurso-saicc" className="font-normal text-yellow-500">
+                    <a 
+                      href="https://github.com/matheusbuttow" 
+                      className="font-normal text-yellow-500"
+                      target="_blank">
                       <GitHubIcon/>
                     </a>
                   </div>
